3284757fed2b53b8cba503bcd60e3a83a0c1805b
zxq <2260391948@qq.com>
2023-07-12 13:57:57 +0800

    公共服务抽离

---
+++ src/main/java/com/xiaozhang/api/mapper/PostThumbMapper.java
@@ -0,0 +1,17 @@
+package com.xiaozhang.api.mapper;
+
+import com.xiaozhang.api.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+//
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ src/main/java/com/xiaozhang/api/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.xiaozhang.api.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xiaozhang.apiCommon.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+//
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
